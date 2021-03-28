c88311ea9350391b338b7003ad00f90eb5a99b0c
Владимир Корнев <78905217+VKornev22@users.noreply.github.com>
2021-03-28 19:09:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-int left = 0, right = size, count = 0;
-while(right > left) {
-int mid = left + (right - left) / 2;
-if(arr[mid] < value)
-left = mid + 1;
-else
-right = mid;
-}
-if(arr[mid] == value) {
-while (arr[mid - 1] == value)
-mid--;
-while (arr[mid] == value) {
-count++;
-left++;
-}
-}
-if(count != 0)
-  return count;
-else
+  int left = 0, right = size, mid, count = 0;
+  while(right > left) {
+  mid = left + (right - left) / 2;
+  if(arr[mid] < value)
+    left = mid + 1;
+  else
+    right = mid;
+  }
+  if(arr[left] == value) {
+  while (arr[left] == value)
+    count++;
+    left++;
+  }
+  }
+  if(count != 0)
+    return count;
+  else
   return 0; // если ничего не найдено
 }
